0fe2fc7eabf51655fa5c1cc5e7c802d46117713d
13018 <13018@10.0.0.99>
2020-05-13 23:10:54 -0400

    Adding New sysout line

--- GitHubProject2/src/com/learning/git/test/HellowWorld.java
+++ GitHubProject2/src/com/learning/git/test/HellowWorld.java
@@ -4,6 +4,7 @@ public class HellowWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hellow World !");
+		System.out.println("Learning Git hub is gettnig interesting");
 
 	}
 
